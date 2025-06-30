d9a7f026e90b0885e77c05a0a10bd923d0aabb6b
Andy Williams <andy@andy.xyz>
2025-06-30 12:56:27 +0100

    Support picom, a new replacement for compton

--- modules/composit/compton.go
+++ modules/composit/compton.go
@@ -25,16 +25,25 @@ func (c *comp) Metadata() fynedesk.ModuleMetadata {
 
 func (c *comp) disable() {
 	_ = exec.Command("killall", "compton").Start()
+	_ = exec.Command("killall", "picom").Start()
 }
 
 func (c *comp) enable() {
+	params := []string{"-c", "-r", "20", "-f", "-i", "1.0", "--vsync"}
+
 	path, err := exec.LookPath("compton")
 	if err != nil {
-		fyne.LogError("Compositor requires compton binary present", err)
-		return
+		path, err = exec.LookPath("picom")
+		if err != nil {
+			fyne.LogError("Compositor requires compton or picom binary present", err)
+			return
+		}
+		params = append(params, "--backend", "glx")
+	} else {
+		params = append(params, "drm", "-C")
 	}
 
-	_ = exec.Command(path, "--vsync", "drm", "-c", "-C", "-r", "20", "-f", "-i", "1.0").Start()
+	_ = exec.Command(path, params...).Start()
 }
 
 // newCompiz creates a new module that will manage composition of the windows.
